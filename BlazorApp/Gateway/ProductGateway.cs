6fab98423e41822273e62527f3757561f8f0aa4d
Joykrisno <97521673+Joykrisno@users.noreply.github.com>
2024-03-21 22:08:05 +0600

    Save Complete

---
+++ BlazorApp/Gateway/ProductGateway.cs
@@ -0,0 +1,25 @@
+﻿using BlazorApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Gateway
+{
+    public class ProductGateway
+    {
+        ApplicationDbContext _db = new ApplicationDbContext();
+
+        public bool Add(Product product)
+        {
+            _db.products.Add(product);
+            int rowAffected = _db.SaveChanges();
+            if(rowAffected>0)
+            {
+                return true;
+            }
+            return false;
+        }
+       
+    }
+}
---
+++ BlazorApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+             optionsBuilder.UseSqlServer("Server=JOY\\MSSQLSERVER01;Database=BlazorAppDb;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<Product> products { get; set; }
+    }
+}
