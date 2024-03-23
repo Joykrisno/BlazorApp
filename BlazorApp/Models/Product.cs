bf0412c7941428a6fee19e5824498f0bbe005fb5
Joykrisno <97521673+Joykrisno@users.noreply.github.com>
2024-03-23 21:48:57 +0600

    Today-Adjust ValidationBlazor

--- BlazorApp/Models/Product.cs
+++ BlazorApp/Models/Product.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorApp.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Type { get; set; }
+       
     }
 }
